ab7dd9e4893680df13c3a60c66cd55255187ce1f
António Teca <antonioteca@hotmail.com>
2023-01-31 15:32:39 +0100

    ultilitario para remover espaço em branco e tranformar o texto em letras minúsculas

--- src/utils/utilitario.js
+++ src/utils/utilitario.js
@@ -1,3 +1,7 @@
 export const currency = function (number) {
     return new Intl.NumberFormat('pt-AO', { style: 'currency', currency: 'AOA', minimumFractionDigits: 2 }).format(number.slice(0, -2));
 };
+
+export const removeSpaceLowerCase  = function(txt) {
+    return txt.replace(/\s/g, "");
+}
