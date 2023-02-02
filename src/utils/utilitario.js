3f6edb11196575ac75d5f55673006619348c7c5e
António Teca <antonioteca@hotmail.com>
2023-02-03 00:05:38 +0100

    importar intl

--- src/utils/utilitario.js
+++ src/utils/utilitario.js
@@ -1,3 +1,5 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-AO';
 export const currency = function (number) {
     return new Intl.NumberFormat('pt-AO', { style: 'currency', currency: 'AOA', minimumFractionDigits: 2 }).format(number.slice(0, -2));
 };
