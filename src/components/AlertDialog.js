b649138ceba47cdbad3327125d7f6b8e20753b09
António Teca <antonioteca@hotmail.com>
2023-03-12 20:28:56 +0100

    alterar alertDialog para adaptar a encomenda e mostrar mensagem

--- src/components/AlertDialog.js
+++ src/components/AlertDialog.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import {StyleSheet} from 'react-native'
+import React from 'react';
+import {StyleSheet, TextInput, ScrollView, View} from 'react-native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import { Button as ButtonUILIB, Constants, Dialog, Text as TextUILIB, View as ViewUILIB, Colors } from 'react-native-ui-lib';
+import { ErroMessage } from './Form';
 
-export const AlertDialog = ({titulo, mensagem, cor})=> {
+export const AlertDialog = ({showDialog, setShowDialog, titulo, mensagem, cor, isEncomenda = false, onPress})=> {
 
-    const [showDialog, setShowDialog] = useState(true);
+    let addressInput = null, informationInput = null;
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
     const renderPannableHeader = props => {
         const {title} = props;
         return (
           <ViewUILIB>
-            <ViewUILIB margin-20>
+            <ViewUILIB margin-15 >
               <TextUILIB $textDefault text65>{title}</TextUILIB>
             </ViewUILIB>
             <ViewUILIB height={2} bg-grey70/>
@@ -19,21 +23,90 @@ export const AlertDialog = ({titulo, mensagem, cor})=> {
     }
 
     return (
-    <Dialog
-      useSafeArea
-      key={1}
-      containerStyle={styles.dialog}
-      visible={showDialog}
-      onDismiss={()=> setShowDialog(false)}
-      renderPannableHeader={renderPannableHeader}
-      pannableHeaderProps={{title: titulo}}
-      supportedOrientations={['portrait', 'landscape']}
-      >
-        <TextUILIB $textDefault margin-20 color = {cor}>{mensagem}</TextUILIB>
-        <ViewUILIB marginB-20 marginR-20 right>
-          <ButtonUILIB label="OK" backgroundColor = {cor} size={ButtonUILIB.sizes.small} onPress={()=> setShowDialog(false)}/>
-        </ViewUILIB>
-    </Dialog>
+    <Formik
+      initialValues={{address: '', telephone: '', information: '' }}
+      validationSchema={Yup.object({
+        address: Yup.string()
+            .max(50,'No máximo 50 caracteres')
+            .required('Digite o seu endereço'),                            
+        telephone: Yup.string()
+            .matches(phoneRegExp, 'Número de telefone não é válido')
+            .min(9,'No mínimo 9 dígitos')
+            .required('Digite o seu número do telefone'),              
+        information: Yup.string()
+            .max(50,'No máximo 50 caracteres'),              
+      })}
+      onSubmit={(values, formikActions) => {
+        setTimeout(() => {
+          // formikActions.setSubmitting(false);
+          onPress(values);
+        }, 500);
+      }}>
+      {props => (
+        <Dialog
+          panDirection='up'
+          containerStyle={styles.dialog}
+          visible={showDialog}
+          onDismiss={()=> setShowDialog(false)}
+          renderPannableHeader={renderPannableHeader}
+          pannableHeaderProps={{title: titulo}}
+          supportedOrientations={['portrait', 'landscape']}>
+          <ScrollView>
+          <View>
+          <TextUILIB $textDefault margin-15 color = {cor}>{mensagem}</TextUILIB>
+          {isEncomenda && 
+          <>
+            <TextUILIB $textDefault marginH-20>*Telefone</TextUILIB>
+            <TextInput
+              editable={!props.isSubmitting}
+              keyboardType='phone-pad'
+              onChangeText={props.handleChange('telephone')}
+              onBlur={props.handleBlur('telephone')}
+              value={props.values.telephone}
+              placeholder="Número de telefone"
+              style={styles.input}
+              onSubmitEditing={() => addressInput.focus()}/>
+            <ViewUILIB marginL-12>
+              <ErroMessage touched={props.touched.telephone} errors={props.errors.telephone}/>
+            </ViewUILIB>
+            <TextUILIB $textDefault marginH-20>*Endereço</TextUILIB>
+            <TextInput
+              editable={!props.isSubmitting}
+              onChangeText={props.handleChange('address')}
+              onBlur={props.handleBlur('address')}
+              value={props.values.address}
+              placeholder="Bairro, Rua, N° Casa..."
+              style={styles.input}
+              onSubmitEditing={() => informationInput.focus()}
+              ref={el => addressInput = el}
+              />
+            <ViewUILIB marginL-12>
+              <ErroMessage touched={props.touched.address} errors={props.errors.address}/>
+            </ViewUILIB>
+            <TextUILIB $textDefault marginH-20>Informações adicionais</TextUILIB>
+            <TextInput
+              editable={!props.isSubmitting}
+              placeholder="Mais detalhes..."
+              style={styles.input}
+              onChangeText={props.handleChange('information')}
+              onBlur={props.handleBlur('information')}
+              value={props.values.information} 
+              ref={el => informationInput = el}
+              />
+            <ViewUILIB marginL-12>
+              <ErroMessage touched={props.touched.information} errors={props.errors.information}/>
+            </ViewUILIB>
+          </>}
+          <ViewUILIB marginB-10 marginR-20 right>
+            {isEncomenda ?
+            <ButtonUILIB label={props.isSubmitting ? 'A encomendar...' : 'Encomendar'} backgroundColor = {cor} size={ButtonUILIB.sizes.medium} disabled={props.isSubmitting} onPress={props.handleSubmit}/>
+            :
+            <ButtonUILIB label={'OK'} backgroundColor = {cor} size={ButtonUILIB.sizes.medium} onPress={()=> setShowDialog(false)}/>}
+          </ViewUILIB>
+          </View>
+          </ScrollView>
+      </Dialog>)}
+    </Formik>
     );
 }
 
@@ -42,5 +115,16 @@ const styles = StyleSheet.create({
       backgroundColor: Colors.$backgroundDefault,
       marginBottom: Constants.isIphoneX ? 0 : 20,
       borderRadius: 12
+    },
+    input: {
+      width: '90%',
+      height: 50,
+      backgroundColor: 'whitesmoke',
+      borderColor: '#ddd',
+      borderWidth: 1,
+      borderRadius: 12,
+      marginHorizontal: 15,
+      marginVertical: 10,
+      padding: 10
     },
 });
