5fbc372fe051d84e1add453aae7a3ff2bd935f5e
António Teca <antonioteca@hotmail.com>
2023-04-10 18:40:46 +0100

    ajustar o width e heightdo search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,27 @@
 import React, { useEffect } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, Dimensions } from "react-native";
 import { Colors } from "react-native-ui-lib";
+import { useHeaderHeight } from '@react-navigation/elements';
 
 const SearchBar = ({searchPhrase, setSearchPhrase, setClicked, inputRef}) => {
-    useEffect(() => {
-      setTimeout(() => inputRef.current.focus(), 500);
-    }, [])
-    return (
-      <TextInput
-        ref={inputRef}
-        style={styles.input}
-        keyboardType='web-search'
-        placeholder="Pesquisa no Mbora"
-        placeholderTextColor='gray'
-        value={searchPhrase}
-        onChangeText={setSearchPhrase}
-        onFocus={()=> setClicked(true)}
-        onBlur={()=> setClicked(false)}
-        // clearButtonMode="always" // IOS
-      />
+  const headerHeight = useHeaderHeight();
+  const { width } = Dimensions.get('window');
+  useEffect(() => {
+    setTimeout(() => inputRef.current.focus(), 500);
+  }, [])
+  return (
+    <TextInput
+      ref={inputRef}
+      style={[styles.input, {width: (width/2) + headerHeight, height: headerHeight/2}]}
+      keyboardType='web-search'
+      placeholder="Pesquisa no Mbora"
+      placeholderTextColor='gray'
+      value={searchPhrase}
+      onChangeText={setSearchPhrase}
+      onFocus={()=> setClicked(true)}
+      onBlur={()=> setClicked(false)}
+      // clearButtonMode="always" // IOS
+    />
   );
 };
 export default SearchBar;
