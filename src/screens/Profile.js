595662bb8a3c2a23103b6bb0b174a1355a26b7c6
António Teca <antonioteca@hotmail.com>
2023-04-28 22:00:24 +0100

    definir tipo de ficheiro a ser seleccionado na galeria

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -201,7 +201,7 @@ export default function Profile({ route, navigation }) {
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 4],
             quality: 0.2,
